c44188233106dd18e45ec7542d6a49045839370c
codecpacka <ramprasad.r2600@gmail.com>
2020-03-19 10:18:16 +0530

    p12

--- P11.java
+++ P11.java
@@ -25,15 +25,32 @@ public class P11 {
         }
 
         // checking odd number of 1 in row
+        System.out.print("row with odd no of 1's:- ");
         for (int i = 0; i < arr.length; i++) {
             int count = 0;
             for (int j = 0; j < arr.length; j++) {
                 if (arr[i][j] == 1)
                     count++;
-
             }
-            if ((count%2)!=0)
+            if (count % 2 != 0) {
+                System.out.print(i + 1 + " ");
+            }
 
+        }
+        System.out.println();
+
+        // checking odd number of is in column
+
+        System.out.print("column with odd no of 1's:- ");
+        for (int j = 0; j < arr.length; j++) {
+            int count = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i][j] == 1)
+                    count++;
+            }
+            if (count % 2 != 0) {
+                System.out.print(j + 1 + " ");
+            }
 
         }
 
