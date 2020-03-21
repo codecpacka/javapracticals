59037185dff45ff7202e6de4712ca71377f252ec
codecpacka <ramprasad.r2600@gmail.com>
2020-03-21 18:26:00 +0530

    e

--- P13.java
+++ P13.java
@@ -12,41 +12,49 @@ public class P13 {
         String eq = tr.replaceAll(" ", "");
         System.out.println(eq);
         int k = 0;
+        int count = 0;
 
         for (int i = 0; i < eq.length(); i++) {
             Character c = Character.valueOf(eq.charAt(i));
             char p = c.charValue();
-            if (p == '+') {
-                System.out.println("+");
+            if (p == '+' || p == '-' || p == '*' || p == '/' || p == '%') {
+                count++;
                 k = i;
             }
         }
-        // int k = Integer.parseInt(c.charValue());/
-        // p13 p=new p13();
-        System.out.println(k);
-        System.out.println(eq.charAt(k));
-        int a = Integer.parseInt(eq.substring(0, k));
-        int b = Integer.parseInt(eq.substring(k + 1));
-        System.out.println(a + " " + b);
+        if (count == 1) {
+            System.out.println("opertor at " + k);
+            System.out.println("operator is " + eq.charAt(k));
+            int a = Integer.parseInt(eq.substring(0, k));
+            int b = Integer.parseInt(eq.substring(k + 1));
+            System.out.println(a + " " + b);
 
-        switch (eq.charAt(k)) {
-            case '+':
-                System.out.println(a + b);
+            switch (eq.charAt(k)) {
+                case '+':
+                    System.out.println(a + b);
 
-                break;
-            case '-':
-                System.out.println(a - b);
-                break;
+                    break;
+                case '-':
+                    System.out.println(a - b);
+                    break;
 
-            case '*':
-                System.out.println(a + b);
-                break;
-            case '/':
-                System.out.println(a / b);
-                break;
+                case '*':
+                    System.out.println(a * b);
+                    break;
+                case '/':
+                    System.out.println(a / b);
+                    break;
+                case '%':
+                    System.out.println(a % b);
+                    break;
 
-            default:
-                break;
+                default:
+                    break;
+            }
+        }
+
+        else {
+            System.out.println("invalid operator");
         }
 
     }
